816b745d0fc8eb486ef96035e9f0a84f1dbf75a9
cgyee <cyee67@gmail.com>
2026-07-11 18:44:08 -0400

    feat: enhance Dashboard with user session, fetch user details, and add logout functionality

--- public/frontend/src/pages/Dashboard.tsx
+++ public/frontend/src/pages/Dashboard.tsx
@@ -1,3 +1,50 @@
-const Dashboard = () => <div>Projects</div>
+import {useEffect, useState} from "react";
+
+type User = {
+    username: string
+    createdAt: string
+}
+
+const logout = async () => {
+    const res = await fetch("/api/logout", {credentials: "include"})
+    if (res.ok) window.location.replace("/login")
+}
+
+const Dashboard = () => {
+    const [user, setUser] = useState<User>()
+    useEffect(() => {
+        (async () => {
+           try {
+               const res = await fetch("/api/me", {credentials: "include"})
+               console.log(res)
+               if (!res.ok) {
+                   window.location.replace("/login")
+                   return
+               }
+               const data = await res.json()
+               console.log(data)
+               setUser(prevState => ({
+                   ...prevState,
+                   username: data.username,
+                   createdAt: data.createdAt,
+               }))
+           } catch (e) {
+               console.error(e)
+           }
+           return
+        })()
+    },[])
+    return (
+        <div>
+            <h1>Dashboard</h1>
+            {user &&
+                <div>
+                    <button onClick={logout}>Logout</button>
+                    <div>Username: {user.username}</div>
+                    <div>Created At: {user.createdAt}</div>
+                </div>}
+        </div>
+    )
+}
 
 export default Dashboard
